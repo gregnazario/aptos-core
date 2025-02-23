0fd4b8790313d4d2b3fe6a1db8e71891e8a80a49
Greg Nazario <greg@gnazar.io>
2025-02-23 17:06:32 -0500

    [release] Skip release check so build can be checked

--- scripts/cli/build_cli_release.sh
+++ scripts/cli/build_cli_release.sh
@@ -10,32 +10,52 @@
 
 set -e
 
+NUM_ARGS="$#"
 NAME='aptos-cli'
 CRATE_NAME='aptos'
 CARGO_PATH="crates/$CRATE_NAME/Cargo.toml"
 PLATFORM_NAME="$1"
 EXPECTED_VERSION="$2"
+SKIP_RELEASE_CHECK="$3"
 
 # Grab system information
 ARCH=$(uname -m)
 OS=$(uname -s)
 VERSION=$(sed -n '/^\w*version = /p' "$CARGO_PATH" | sed 's/^.*=[ ]*"//g' | sed 's/".*$//g')
 
+echo "Building for Architecture: '$ARCH' OS: '$OS' Platform: '$PLATFORM_NAME' Version: '$VERSION'"
+
+if [ "$NUM_ARGS" -eq 0 ]; then
+  echo "No arguments provided. Please provide platform name then expected version.";
+  exit 120;
+elif [ "$NUM_ARGS" -eq 2 ]; then
+  echo "Checking if CLI version was already released."
+  # Do nothing
+elif [ "$NUM_ARGS" -eq 3 ]; then
+  # This is skipping release check
+  echo "Skipping check if CLI version was already released."
+else
+  echo "Wrong number of arguments provided. Please provide platform name then expected version."
+  exit 121
+fi
+
 # Check that the version is well-formed, note that it should already be correct, but this double checks it
 if ! [[ "$EXPECTED_VERSION" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-  echo "$EXPECTED_VERSION is malformed, must be of the form '^[0-9]+\.[0-9]+\.[0-9]+$'"
+  echo "Inputted version '$EXPECTED_VERSION' is malformed, must be of the form '^[0-9]+\.[0-9]+\.[0-9]+$'"
   exit 1
 fi
 
 # Check that the version matches the Cargo.toml
 if [[ "$EXPECTED_VERSION" != "$VERSION" ]]; then
-  echo "Wanted to release for $EXPECTED_VERSION, but Cargo.toml says the version is $VERSION"
+  echo "Wanted to release for '$EXPECTED_VERSION', but Cargo.toml says the version is '$VERSION'"
   exit 2
 fi
 
-# Check that the release doesn't already exist
-if curl -s --stderr /dev/null --output /dev/null --head -f "https://github.com/aptos-labs/aptos-core/releases/download/aptos-cli-v$EXPECTED_VERSION/aptos-cli-$EXPECTED_VERSION-Ubuntu-22.04-x86_64.zip"; then
-  echo "$EXPECTED_VERSION already released"
+# Check that the release doesn't already exist (we're using Linux from now on)
+export CURL_RESP=`curl -s --stderr /dev/null --output /dev/null --head -f "https://github.com/aptos-labs/aptos-core/releases/download/aptos-cli-v$EXPECTED_VERSION/aptos-cli-$EXPECTED_VERSION--x86_64.zip"`
+
+if [[ -z "$CURL_RESP" && -z "$SKIP_RELEASE_CHECK" ]]; then
+  echo "Version '$EXPECTED_VERSION' already released"
   exit 3
 fi
 
