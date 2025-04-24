c5ca928fd5f5cb293d6c9535082c93647ec6ddbe
Greg Nazario <greg@gnazar.io>
2025-04-24 00:16:46 -0400

    fix rustup

--- scripts/cli/minimal_cli_build.sh
+++ scripts/cli/minimal_cli_build.sh
@@ -17,6 +17,10 @@ else
 fi
 
 # TODO: Do we need to add `ca-certificates`, `curl`, `unzip`, `wget`
+# Install rustup
+if ! command -v rustup&>/dev/null; then
+  curl https://sh.rustup.rs -sSf | sh -s -- -y
+fi
 
 OS="$(uname)"
 case "$OS" in
@@ -24,26 +28,24 @@ case "$OS" in
     if command -v apt-get &>/dev/null; then
       # Ubuntu / Debian based APT-GET
       sudo apt-get update
-      sh install_pkg.sh build-essential pkgconf libssl-dev git rustup libudev-dev lld libdw-dev clang llvm cmake
+      sh install_pkg.sh build-essential pkgconf libssl-dev git libudev-dev lld libdw-dev clang llvm cmake
     elif command -v dnf &>/dev/null; then
       # RHEL / CentOS based DNF
-      sh install_pkg.sh gcc make pkgconf openssl-devel git rustup rust-libudev-devel lld elfutils-devel clang llvm cmake
+      sh install_pkg.sh gcc make pkgconf openssl-devel git rust-libudev-devel lld elfutils-devel clang llvm cmake
       sudo dnf install gcc-c++ # TODO: There's something wrong with install_pkg for this one
-      /usr/bin/rustup-init -y
     elif command -v yum &>/dev/null; then
       # RHEL / CentOS based YUM
-      sh install_pkg.sh gcc gcc-c++ make pkgconf openssl-devel git rustup rust-libudev-devel lld elfutils-devel clang llvm cmake
+      sh install_pkg.sh gcc gcc-c++ make pkgconf openssl-devel git rust-libudev-devel lld elfutils-devel clang llvm cmake
       sudo yum install gcc-c++ # TODO: There's something wrong with install_pkg for this one
-      /usr/bin/rustup-init -y
     elif command -v pacman &>/dev/null; then
       # Arch based PACMAN
-      sh install_pkg.sh base-devel pkgconf openssl git rustup lld clang llvm cmake
+      sh install_pkg.sh base-devel pkgconf openssl git lld clang llvm cmake
     elif command -v apk &>/dev/null; then
       # Alpine based APK
-      sh install_pkg.sh alpine-sdk coreutils pkgconfig openssl-dev git rustup lld elfutils-dev clang llvm cmake libc-dev
+      sh install_pkg.sh alpine-sdk coreutils pkgconfig openssl-dev git lld elfutils-dev clang llvm cmake libc-dev
     elif command -v zypper &>/dev/null; then
       # OpenSUSE zypper
-      sh install_pkg.sh gcc make pkg-config libopenssl-devel git rustup libudev-devel lld libdw-devel clang llvm cmake
+      sh install_pkg.sh gcc make pkg-config libopenssl-devel git libudev-devel lld libdw-devel clang llvm cmake
       sudo zypper install gcc-c++ # TODO: There's something wrong with install_pkg for this one
     #elif command -v emerge &>/dev/null; then
       # Gentoo Emerge
@@ -52,7 +54,7 @@ case "$OS" in
     #  sh install_pkg.sh --skip-overrides sys-devel/gcc dev-libs/openssl dev-vcs/git dev-lang/rust
     elif command -v xbps-install &>/dev/null; then
       # Void linux xbps
-      sh install_pkg.sh gcc make pkg-config git rustup lld elfutils-devel clang llvm cmake
+      sh install_pkg.sh gcc make pkg-config git lld elfutils-devel clang llvm cmake
     else
       # TODO: Support more package managers?
       echo "Unable to find supported package manager (apt-get, dnf, yum, zypper, xbps or pacman). Abort"
@@ -62,7 +64,10 @@ case "$OS" in
   Darwin)
     # TODO: May need to do a different path for macports, but atm brew is expected here
     # TODO: May need other installation tools
-    sh install_pkg.sh pkgconfig openssl git rustup llvm cmake
+    sh install_pkg.sh pkgconfig openssl git llvm cmake
+  ;;
+  FreeBSD)
+    sh install_pkg.sh gcc gmake binutils pkgconf git openssl cmake llvm
   ;;
   *)
     echo "Unknown OS. Abort."
